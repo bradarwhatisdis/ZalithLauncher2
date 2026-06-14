ba3b9217125fe3b811e5f4adde7c4d21bd61dbef
bradarwhatisdis <bebelalapim@gmail.com>
2026-06-14 09:36:57 +0000

    chore: rename keystore to open_launcher.jks

--- ZalithLauncher/build.gradle.kts
+++ ZalithLauncher/build.gradle.kts
@@ -46,9 +46,9 @@ android {
 
     signingConfigs {
         create("releaseBuild") {
-            storeFile = file("zalith_launcher.jks")
+            storeFile = file("open_launcher.jks")
             storePassword = getKeyFromLocal("STORE_PASSWORD", ".store_password.txt")
-            keyAlias = "movtery_zalith"
+            keyAlias = "open_launcher"
             keyPassword = getKeyFromLocal("KEY_PASSWORD", ".key_password.txt")
         }
         create("debugBuild") {
